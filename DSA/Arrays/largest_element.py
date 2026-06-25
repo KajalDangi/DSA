2af4d993726f2504e9f7f22ef99fca267347c062
KAJAL <kajaldangithakur@gmail.com>
2026-06-25 21:37:11 +0530

    "Striver A2Z Arrays: Largest Element and Second Largest Element"

---
+++ DSA/Arrays/largest_element.py
@@ -0,0 +1,45 @@
+# Given an array of integers nums, return the largest element.
+
+# --------------------------
+# Approach 1: Sorting
+# Time: O(n log n)
+# Space: O(n)
+# Better
+# --------------------------
+
+def largestElement(nums):
+    sort_num = sorted(nums)
+    return sort_num[-1]
+
+
+
+# --------------------------
+# Approach 2: max() Function
+# Time: O(n)
+# Space: O(1)
+# Library Function
+# --------------------------
+def largestElement(nums):
+    return max(nums)
+
+
+
+# --------------------------
+# Approach 3: Linear Scan
+# Time: O(n)
+# Space: O(1)
+# Optimal
+# --------------------------
+def largestElement(nums):
+    largest = nums[0]
+    for num in nums:
+        if num > largest:
+            largest = num
+    return largest
+
+
+
+# --------------------------
+if __name__ == "__main__":
+    nums = [3, 3, 6, 1]
+    print(largestElement(nums))   # Output: 6
---
+++ DSA/Arrays/second_largest_element.py
@@ -0,0 +1,44 @@
+# Given an array of integers nums, return the second-largest element in the array.
+# If the second-largest element does not exist, return -1.
+
+# --------------------------
+# Approach 1: Sorting and set
+# Time: O(n log n)
+# Space: O(n)
+# Brute
+# --------------------------
+def secondLargestElement(nums):
+     temp_nums = sorted(list(set(nums)))
+     if len(temp_nums) > 1:
+         return temp_nums[-2]
+     else:
+         return -1
+
+# --------------------------
+# Approach 2: max() Function and linear scan
+# Time: O(n)
+# Space: O(1)
+# Better
+# --------------------------
+def secondLargestElement(nums):
+    largest = max(nums)
+    second_largest = -1
+    for num in nums:
+        if num > second_largest and num != largest:
+            second_largest = num
+    return second_largest
+
+
+
+# --------------------------
+# Approach 3:
+# Time:
+# Space:
+# --------------------------
+
+# code
+
+# --------------------------
+if  __name__ == "__main__":
+    nums = [8, 8, 7, 6, 5]
+    print(secondLargestElement(nums)) #Output = 7
